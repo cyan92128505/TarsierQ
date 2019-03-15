22119ab18d2edc0c88e54a745ee9fa665aa7bb05
cyan92128505 <cyan92128505@gmail.com>
2019-03-15 17:21:55 +0800

    Update api

--- index.js
+++ index.js
@@ -3,84 +3,123 @@ const os = require('os');
 const path = require('path');
 
 const express = require('express');
-const Datastore = require('nedb');
 const bodyParser = require('body-parser');
-const { qrcode } = require('pure-svg-code');
+
+const clientList = [
+    {
+        deviceId: 0,
+        token: '',
+        login: false,
+    },
+];
 
 const app = express();
 
 app.set('view engine', 'ejs');
-const db = new Datastore('server.db');
-db.loadDatabase(() => exec());
 
-function exec() {
-    db.findOne().exec((err, docs) =>
-        err != null ? console.log(`ERROR: ${err}`) : console.log(`DB: ${docs}`)
-    );
+const server = app.listen(3000);
 
-    var user = null;
+const io = require('socket.io')(server, {
+    transports: ['websocket'],
+});
 
-    const server = app.listen(3000);
-
-    const io = require('socket.io')(server, {
-        transports: ['websocket'],
+io.sockets.on('connection', function(socket) {
+    socket.emit('sendMessage', { hello: 'world' });
+    socket.on('sendMessage', function(data) {
+        user = data;
+        console.log(data);
     });
+});
 
-    io.sockets.on('connection', function(socket) {
-        socket.emit('sendMessage', { hello: 'world' });
-        socket.on('sendMessage', function(data) {
-            user = data;
-            console.log(data);
-        });
+app.use('/static', express.static(path.join(process.cwd(), 'asset')));
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    const ifaces = os.networkInterfaces();
+    const url = `http://${
+        !!ifaces.eth0 ? ifaces.eth0[0].address : '192.168.88.194'
+    }:3000`;
+
+    res.render(path.join(process.cwd(), 'view', 'index.ejs'), {
+        url: encodeURIComponent(url),
+        rawUrl: url,
     });
+});
 
-    app.use('/static', express.static(path.join(process.cwd(), 'asset')));
-    app.use(
-        bodyParser.urlencoded({
-            extended: true,
+app.get('/client', (req, res, next) => {
+    res.json({
+        clientList: clientList.filter(c => c.deviceId != 0),
+    });
+});
+
+app.get('/clear', () => {
+    clientList = clientList.filter(c => c.deviceId === 0);
+    res.json({
+        clientList: clientList,
+    });
+});
+
+app.post('/generator', (req, res, next) => {
+    const _deviceId = req.body.deviceId || null;
+    const _user = req.body.hash;
+    if (!_deviceId) {
+        console.log('No deviceId');
+        res.send(`ERROR to generate`);
+        return;
+    }
+
+    if (clientList.some(e => e.deviceId === _deviceId)) {
+        console.log('Exist deviceId');
+        res.send(`ERROR to generate`);
+        return;
+    }
+
+    const secret = 'tarsier';
+    const hash = crypto
+        .createHmac('sha256', secret)
+        .update(req.body.deviceId)
+        .digest('hex');
+
+    const client = {
+        deviceId: req.body.deviceId,
+        token: hash,
+    };
+
+    clientList.push(client);
+
+    io.to(_user).emit('sendMessage', client);
+
+    const result = `{"token": "${hash}", "user": "${_user}"}`;
+    console.log(result);
+    res.send(result);
+});
+
+app.post('/login/:deviceId', (req, res, next) => {
+    const _deviceId = req.params.deviceId;
+    const _user = req.body.hash;
+    const _token = req.body.token;
+
+    if (
+        !clientList.some(c => {
+            const state = c.deviceId === _deviceId && c.token === _token;
+            if (state) {
+                c.login = true;
+            }
+            return state;
         })
-    );
+    ) {
+        console.log(`ERROR to login`);
+        res.send(`ERROR to login`);
+        return;
+    }
 
-    app.use(bodyParser.json());
-
-    app.get('/', function(req, res) {
-        const ifaces = os.networkInterfaces();
-        const ip = `http://${
-            !!ifaces.eth0 ? ifaces.eth0[0].address : '192.168.88.194'
-        }:3000/generator`;
-        const secret = 'tarsier';
-        const hash = crypto
-            .createHmac('sha256', secret)
-            .update('/ac001/user')
-            .digest('hex');
-
-        const svgString = qrcode(`{\"url\": \"${ip}\",\"hash\": \"${hash}\"}`);
-        res.render(path.join(process.cwd(), 'view', 'index.ejs'), {
-            svgString: svgString,
-            url: encodeURIComponent(ip),
-            hash: hash,
-        });
-    });
-
-    app.post('/generator', (req, res, next) => {
-        const secret = 'tarsier';
-        const hash = crypto
-            .createHmac('sha256', secret)
-            .update(req.body.deviceId)
-            .digest('hex');
-
-        if (user) {
-            io.to(user).emit('sendMessage', {
-                deviceId: req.body.deviceId,
-            });
-        }
-
-        res.send(hash);
-    });
-
-    app.post('/tarsier-login/:deviceId', (req, res, next) => {
-        const _deviceId = req.params.deviceId;
-        const _seedId = req.body.seedId;
-        res.send(_deviceId);
-    });
-}
+    const result = `token: ${_token}, user: ${_user}`;
+    console.log(result);
+    res.send(result);
+});
